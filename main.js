24e9cc63eb361e21f905e1e6fcbf1b70963dbee8
Akash Gvalani <agvalani@villanova.edu>
2018-07-01 23:38:27 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
 const cities = require('cities');
-const url = require('url');
-const http = require('http');
-const app = http.createServer((request, response) = &gt; {
-            var city, query;
-            query = url.parse(request.url,true).query;
-            if(query.zipCode) city = cities.zip_lookup(query.zipCode).city; //looking for query results from a zip code
-            else city = "not found";
-            response.writeHead(200, {"Content-Type":"Text/HTML"});
-            response.Write('&lt;h1&gt;The city you are in is ${city}.&lt;h1&gt;');
-            response.end();
-            });
-
-app.listen(3000);
+var myCity = cities.zip_lookup(10016);
+console.console.log(myCity);
---
+++ messages.js
@@ -0,0 +1,2 @@
+let messages = ["You are great and a very big chutya!!", "Success is in your hands"];
+exports.messages = ["Hi there", "I want to say I am really enjoying working with Atom", "I think its a fine invention by developers!"];
